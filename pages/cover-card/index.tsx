51648ebb26c8688c0e6ba6f4cff186e476ae38e8
mannmehta_crest <mannmehta2199@gmail.com>
2021-06-29 19:23:36 +0530

    Testing

--- pages/cover-card/index.tsx
+++ pages/cover-card/index.tsx
@@ -1,4 +1,21 @@
+import React from 'react';
+
 function coverCard() {
-  return (<div>Hello</div>);
+  const [touchStart, setTouchStart] = React.useState(0);
+  const [touchEnd, setTouchEnd] = React.useState(0);
+
+  const handleTouchStart = (e: any) => {
+    console.log('Hello', e);
+    setTouchStart(e.targetTouches[0].clientY);
+  }
+  const handleTouchEnd = (e: any) => {
+    console.log('Hello', e);
+    setTouchEnd(e.targetTouches[0].clientY);
+    alert("Yes");
+  };
+  return (
+    <div onTouchStart={handleTouchStart} onTouchEnd={handleTouchEnd} style={{ height: '100vh', width: '100%', backgroundColor: 'black' }}>
+    </div>
+  );
 }
 export default coverCard;
